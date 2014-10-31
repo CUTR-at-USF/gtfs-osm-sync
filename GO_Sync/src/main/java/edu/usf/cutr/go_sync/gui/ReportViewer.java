de56515f6622c73fa6b91a0038707aa4cad3e13b
Reuben Peterkin <reuben_p@yahoo.com>
2014-10-31 20:36:14 +1000

    Fix Bus Stop icon
    On branch master
    	GO_Sync/.classpath
    nothing added to commit but untracked files present (use "git add" to track)

--- GO_Sync/src/main/java/edu/usf/cutr/go_sync/gui/ReportViewer.java
+++ GO_Sync/src/main/java/edu/usf/cutr/go_sync/gui/ReportViewer.java
@@ -632,12 +632,27 @@ public class ReportViewer extends javax.swing.JFrame implements TableModelListen
         stopsPainter.setWaypoints(waypoints);
 
         stopsPainter.setRenderer(new DefaultWaypointRenderer() {
-                public boolean paintWaypoint(Graphics2D g, JXMapViewer map, JXMapViewer v, Waypoint wp) {
-                Image busIcon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/edu/usf/cutr/go_sync/gui/bus_icon.png")); //Toolkit.getDefaultToolkit().getImage("bus_icon.png");
-                g.drawImage(busIcon, -5, -5, map);
-                return true;
-            }
-        });
+            public boolean paintWaypoint(Graphics2D g, JXMapViewer map, JXMapViewer v, Waypoint wp) {
+            Image busIcon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/edu/usf/cutr/go_sync/gui/bus_icon.png")); //Toolkit.getDefaultToolkit().getImage("bus_icon.png");
+            g.drawImage(busIcon, -5, -5, map);
+            return true;
+        }
+            public void paintWaypoint(Graphics2D g, JXMapViewer map, Waypoint wp) {
+            Image busIcon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/edu/usf/cutr/go_sync/gui/bus_icon.png")); //Toolkit.getDefaultToolkit().getImage("bus_icon.png");
+    		Point2D point = map.getTileFactory().geoToPixel(wp.getPosition(), map.getZoom());
+    		
+    		int x = (int)point.getX() -16/ 2;
+    		int y = (int)point.getY() -16;
+    		
+            g.drawImage(busIcon,x, y, map);
+            return;
+        }
+//			public void paintWaypoint(Graphics2D g, JXMapViewer map,Object waypoint) {
+//                Image busIcon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/edu/usf/cutr/go_sync/gui/bus_icon.png")); //Toolkit.getDefaultToolkit().getImage("bus_icon.png");
+//                g.drawImage(busIcon, -5, -5, map);			
+//                System.out.println(busIcon.g);
+//			}                
+    });
 
         mainMap.setZoom(1);
         mainMap.calculateZoomFrom(allStopsGeo);
@@ -666,9 +681,9 @@ public class ReportViewer extends javax.swing.JFrame implements TableModelListen
             public void mouseClicked(MouseEvent e){
                 Stop selected = findStop(e.getPoint());
                 if(selected!=null){
-                    System.out.println(selected);
+                    System.out.println(selected+" " + selected.getReportCategory() + " MouseListener.mouseClicked");
                     System.out.println(selected.getLat()+","+selected.getLon());
-
+                    
                     // when the user is in multiple possible match category
                     // if user selects one of the items in osm list then only update osm list, no need to zoom in
                     boolean isInOsmList = false;

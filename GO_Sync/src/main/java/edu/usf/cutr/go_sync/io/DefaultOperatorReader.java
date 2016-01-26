a342d8891577f028eeba769f5de416684717b163
Reuben Peterkin <reuben_p@yahoo.com>
2016-01-26 10:32:38 +1000

    Split DefaultOperatorReader

--- GO_Sync/src/main/java/edu/usf/cutr/go_sync/io/DefaultOperatorReader.java
+++ GO_Sync/src/main/java/edu/usf/cutr/go_sync/io/DefaultOperatorReader.java
@@ -18,76 +18,86 @@ import java.util.List;
  */
 public class DefaultOperatorReader {
 
-        public List<DefaultOperator> readOperators(String fName){
-       
+	
+	private List<DefaultOperator> readOperatorsInternal  (BufferedReader br) throws IOException
+	{
         String thisLine;
         String [] elements;
         List<DefaultOperator> ops = new ArrayList<DefaultOperator>();
+
+        boolean isFirstLine = true;
+        while ((thisLine = br.readLine()) != null) { 
+            if (isFirstLine) {
+                isFirstLine = false;
+            }
+            else {
+                thisLine = thisLine.trim();
+                
+                if(thisLine.contains("\"")) {
+                     String[] temp = thisLine.split("\"");
+                     ArrayList l = new ArrayList();
+                     l.add(temp[1].toString());
+                     int commas = temp[1].replaceAll("[^,]", "").length();
+                     
+                     elements = thisLine.split(",");
+                     
+                     for(int i = 0; i < elements.length; i++) {
+                         if(i > commas) {
+                             l.add(elements[i].toString());
+                         }
+                     }
+                     elements = (String[]) l.toArray(new String[0]);
+                } 
+                else {
+                    elements = thisLine.split(",");    
+                }
+                
+                
+                DefaultOperator op = new DefaultOperator(elements[0]);
+                
+                System.out.print(":: ");
+                for(int i = 0; i < elements.length; i++) {
+                    System.out.print(elements[i] + " : ");
+                    
+                    if(i == 1 && elements[1] != null) {
+                        op.setOperatorAbbreviation(elements[1]);
+                    }
+
+                    if(i == 2 && elements[2] != null) {
+                        try{
+                        op.setNtdID(Integer.parseInt(elements[2]));
+                        } catch(NumberFormatException ex) {
+                            System.err.println("Error parsing NTD ID " + elements[2] + " - " + ex.getLocalizedMessage());
+                        }
+                    }
+                    
+                    if(i == 3 && elements[3] != null) {
+                        op.setGtfsURL(elements[3]);
+                    }
+                    
+                    if(i == 4 && elements[4] != null) {
+                        try{
+                        op.setStopDigits(Integer.parseInt(elements[4]));
+                        } catch(NumberFormatException ex) {
+                            System.err.println("Error parsing stop digits " + elements[4] + " - " + ex.getLocalizedMessage());
+                        }
+                    }
+                }
+                System.out.println(" :: ");
+                
+                ops.add(op);
+            }
+        } 
+        return ops;
+	}
+	
+        public List<DefaultOperator> readOperators(String fName){
+       
+
         try {
             BufferedReader br = new BufferedReader(new FileReader(fName));
-            boolean isFirstLine = true;
-            while ((thisLine = br.readLine()) != null) { 
-                if (isFirstLine) {
-                    isFirstLine = false;
-                }
-                else {
-                    thisLine = thisLine.trim();
-                    
-                    if(thisLine.contains("\"")) {
-                         String[] temp = thisLine.split("\"");
-                         ArrayList l = new ArrayList();
-                         l.add(temp[1].toString());
-                         int commas = temp[1].replaceAll("[^,]", "").length();
-                         
-                         elements = thisLine.split(",");
-                         
-                         for(int i = 0; i < elements.length; i++) {
-                             if(i > commas) {
-                                 l.add(elements[i].toString());
-                             }
-                         }
-                         elements = (String[]) l.toArray(new String[0]);
-                    } 
-                    else {
-                        elements = thisLine.split(",");    
-                    }
-                    
-                    
-                    DefaultOperator op = new DefaultOperator(elements[0]);
-                    
-                    System.out.print(":: ");
-                    for(int i = 0; i < elements.length; i++) {
-                        System.out.print(elements[i] + " : ");
-                        
-                        if(i == 1 && elements[1] != null) {
-                            op.setOperatorAbbreviation(elements[1]);
-                        }
-
-                        if(i == 2 && elements[2] != null) {
-                            try{
-                            op.setNtdID(Integer.parseInt(elements[2]));
-                            } catch(NumberFormatException ex) {
-                                System.err.println("Error parsing NTD ID " + elements[2] + " - " + ex.getLocalizedMessage());
-                            }
-                        }
-                        
-                        if(i == 3 && elements[3] != null) {
-                            op.setGtfsURL(elements[3]);
-                        }
-                        
-                        if(i == 4 && elements[4] != null) {
-                            try{
-                            op.setStopDigits(Integer.parseInt(elements[4]));
-                            } catch(NumberFormatException ex) {
-                                System.err.println("Error parsing stop digits " + elements[4] + " - " + ex.getLocalizedMessage());
-                            }
-                        }
-                    }
-                    System.out.println(" :: ");
-                    
-                    ops.add(op);
-                }
-            } 
+            List<DefaultOperator> ops = readOperatorsInternal(br);
+            br.close();
             return ops;
         }
         catch (IOException e) {
